9a7d5504fceed6f92cb3b184244c70d4e0cbc3ef
Chung Poon <cmpoon212@gmail.com>
2016-03-20 18:47:32 +0000

    Removed Barcode

--- resources/views/awards/index.blade.php
+++ resources/views/awards/index.blade.php
@@ -5,23 +5,23 @@
 
     <div class="ui-grid-a" style="text-align:center">
         <div class="ui-block-a">
-            <img src="/assets/images/badges/bigsaverday-{{ $saver ? 'on' : 'off'  }}.png" alt="" style="">
+            <img src="/assets/images/badges/bigsaverday-{{ $saver ? 'on' : 'off'  }}.png" alt="" style="width:150px">
 
             <p><strong>Big Saver Day</strong></p>
         </div>
         <div class="ui-block-b">
-            <img src="/assets/images/badges/cheapshopperweek-{{ $cheapshopper ? 'on' : 'off'  }}.png" alt="" style="">
+            <img src="/assets/images/badges/cheapshopperweek-{{ $cheapshopper ? 'on' : 'off'  }}.png" alt="" style="width:150px">
 
             <p><strong>Cheap Shopper Week</strong></p>
         </div>
         <div class="ui-block-a">
-            <img src="/assets/images/badges/crazycollector-{{ $crazycollector ? 'on' : 'off'  }}.png" alt="" style="">
+            <img src="/assets/images/badges/crazycollector-{{ $crazycollector ? 'on' : 'off'  }}.png" alt="" style="width:150px">
 
             <p><strong>Crazy Collector</strong></p>
         </div>
         <div class="ui-block-b">
             <img src="/assets/images/badges/projectionperfection-{{ $projectionperfection ? 'on' : 'off'  }}.png" alt=""
-                 style="">
+                 style="width:150px">
 
             <p><strong>Projection Perfection</strong></p>
         </div>
